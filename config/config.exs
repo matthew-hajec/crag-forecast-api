4186a1ef073a5d40bced188bceefe63ad0b22ad0
matthew-hajec <matt.hajec@pm.me>
2025-10-23 23:14:59 -0400

    feat: update configuration files for runtime environment and add CORS support

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,6 @@ import Config
 # Shared configuration for all environments
 config :crag_forecast,
   # Default values that can be overridden by environment-specific configs
-  port: 4000,
   use_dev_routes: false,
   weather_provider: CragForecast.WeatherProviders.OpenMeteo,
   crag_loader: CragForecast.CragLoaders.WebCSV,
@@ -11,11 +10,9 @@ config :crag_forecast,
   forecast_provider: CragForecast.ForecastProviders.DefaultProvider,
   ecto_repos: [CragForecast.Repo]
 
-config :crag_forecast, CragForecast.CragLoaders.WebCSV,
-  url:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQX23tF6CLyQHv8FtCQ54MdGeTtQb-gMNgMpOE-lCiZS4uP9D-6OaswesxP4M2oZrZuJrW7PMThYEJb/pub?output=csv"
-
-config :crag_forecast, CragForecast.HTTP, max_radius_km: 1000
+config :crag_forecast, CragForecast.HTTP,
+  max_radius_km: 1000,
+  cors_allowed_origins: "*"
 
 config :crag_forecast, CragForecast.Repo,
   database: ":memory:",
--- config/prod.exs
+++ config/prod.exs
@@ -2,13 +2,4 @@ import Config
 
 # Production environment configuration
 config :crag_forecast,
-  port: String.to_integer(System.get_env("PORT") || "4000"),
   use_dev_routes: false
-
-config :crag_forecast, CragForecast.CragLoaders.WebCSV,
-  url:
-    System.get_env("CRAG_CSV_URL") ||
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vQX23tF6CLyQHv8FtCQ54MdGeTtQb-gMNgMpOE-lCiZS4uP9D-6OaswesxP4M2oZrZuJrW7PMThYEJb/pub?output=csv"
-
-# Configure production logging level
-config :logger, level: :info
