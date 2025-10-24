d9fe8252393d5115e33ed2a52cd0f375df00610c
matthew-hajec <matt.hajec@pm.me>
2025-10-24 13:21:25 -0400

    fix: fix error logging for weather retreival

--- lib/crag_forecast/forecast_providers/default_provider.ex
+++ lib/crag_forecast/forecast_providers/default_provider.ex
@@ -26,7 +26,7 @@ defmodule CragForecast.ForecastProviders.DefaultProvider do
             {:ok, %{crag: crag, weather_window: weather_window}}
 
           {:error, reason} ->
-            Logger.warning("Failed to fetch weather info for: #{crag}")
+            Logger.warning("Failed to fetch weather info for: #{crag.name} at (#{crag.latitude}, #{crag.longitude}): #{inspect(reason)}")
             {:error, reason}
         end
       end)
